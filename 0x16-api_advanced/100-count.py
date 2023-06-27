bdd5e4bddfeabb5ada320ecd8532c670ea18e315
KrizTech <christiandonatus01@gmail.com>
2023-06-26 17:45:05 -0700

    API Advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,54 +1,67 @@
 #!/usr/bin/python3
-""" raddit api"""
-
-import json
+"""This module queries the reddit API"""
 import requests
+from sys import argv
 
 
-def count_words(subreddit, word_list, after="", count=[]):
-    """count all words"""
-
-    if after == "":
-        count = [0] * len(word_list)
-
+def recurse_count(subreddit, hot_list=[], after=None):
+    """This function queries the reddit API recursively
+    """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    request = requests.get(url,
-                           params={'after': after},
-                           allow_redirects=False,
-                           headers={'user-agent': 'bhalut'})
+    payload = {"after": after, "limit": 100}
+    headers = {"User-Agent": "Python/requests"}
 
-    if request.status_code == 200:
-        data = request.json()
-
-        for topic in (data['data']['children']):
-            for word in topic['data']['title'].split():
-                for i in range(len(word_list)):
-                    if word_list[i].lower() == word.lower():
-                        count[i] += 1
-
-        after = data['data']['after']
-        if after is None:
-            save = []
-            for i in range(len(word_list)):
-                for j in range(i + 1, len(word_list)):
-                    if word_list[i].lower() == word_list[j].lower():
-                        save.append(j)
-                        count[i] += count[j]
-
-            for i in range(len(word_list)):
-                for j in range(i, len(word_list)):
-                    if (count[j] > count[i] or
-                            (word_list[i] > word_list[j] and
-                             count[j] == count[i])):
-                        aux = count[i]
-                        count[i] = count[j]
-                        count[j] = aux
-                        aux = word_list[i]
-                        word_list[i] = word_list[j]
-                        word_list[j] = aux
-
-            for i in range(len(word_list)):
-                if (count[i] > 0) and i not in save:
-                    print("{}: {}".format(word_list[i].lower(), count[i]))
+    try:
+        req = requests.get(url, headers=headers, params=payload,
+                           allow_redirects=False)
+        if req.status_code == 200:
+            data = req.json()
+            after = data.get("data")["after"]
+            for post in data.get("data")["children"]:
+                hot_list.append(post.get("data")["title"])
+            if after:
+                return recurse_count(subreddit, hot_list, after)
+            else:
+                return hot_list
         else:
-            count_words(subreddit, word_list, after, count)
+            return None
+    except requests.exceptions.JSONDecodeError:
+        pass
+
+
+def count_words(subreddit, word_list):
+    """This function queries the reddit API and
+    sorts a list of words by occurences
+    """
+    word_dict = {}
+
+    all_titles = recurse_count(subreddit)
+    word_list = [w.lower() for w in word_list]
+
+    # Only parse responses that not None
+    if all_titles:
+        for word in word_list:
+            count = 0
+            for title in all_titles:
+
+                # convert words to lowercase for comparison
+                title = [w.lower() for w in title.split()]
+
+                # Only count for present words in response
+                if word in title:
+                    for w in title:
+                        if word == w:
+                            count += 1
+            # Only add words that are present to dictionary
+            if count:
+
+                """If a word is duplicated in the function parameter
+                add all the occurrences
+                """
+                if word_dict.get(word):
+                    count += word_dict[word]
+                word_dict[word] = count
+        sorted_dict = dict(sorted(word_dict.items(),
+                           key=lambda item: item[1], reverse=True))
+        for k, v in sorted_dict.items():
+            print("{}: {:d}".format(k, v))
